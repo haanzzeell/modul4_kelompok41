b509b2a60d21d20e872b6b6a57dfe203a432001e
haanzzeell <hanaadilahh@gmail.com>
2020-04-23 00:58:56 +0700

    Tugas_modul4

---
+++ Tugas_Modul4_Kel41/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Tugas_Modul4_Kel41
+{
+    class Program
+    {
+        static void keterangan(string nama, string nim)
+        {
+            Console.WriteLine("Nama: " + nama + " NIM " + nim);
+        }
+        static void keterangan2(int kelompok, int shift)
+        {
+            Console.WriteLine("Kelompok " + kelompok + " shift ke-" + shift);
+        }
+
+        static void Main(string[] args)
+        {
+
+            string a;
+            int b;
+            hae hr = new hae();
+
+            do
+            {
+                Console.WriteLine("============================================");
+
+                Console.WriteLine("Senin   = 1                              {#}");
+                Console.WriteLine("Selasa  = 2                              {#}");
+                Console.WriteLine("Rabu    = 3                              {#}");
+                Console.WriteLine("Kamin   = 4                              {#}");
+                Console.WriteLine("Jum'at  = 5                              {#}");
+                Console.WriteLine("============================================");
+                Console.Write("Pilih hari :");
+                a = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("============================================");
+                int.TryParse(a, out b);
+                switch (b)
+                {
+                    case (1):
+                        hr.senin();
+                        break;
+                    case (2):
+                        hr.selasa();
+                        break;
+                    case (3):
+                        hr.rabu();
+                        break;
+                    case (4):
+                        hr.kamis();
+                        break;
+                    case (5):
+                        hr.jumat();
+                        break;
+                    default:
+                        Console.WriteLine("Inputan salah");
+                        break;
+
+                }
+                Console.WriteLine("============================================");
+                Console.WriteLine("");
+                Console.WriteLine("Apakah Ingin melihat jadwal hari lain? (ya)");
+                a = Console.ReadLine();
+                Console.WriteLine("____________________________________________");
+                Console.WriteLine("");
+
+
+            }
+            while (a == "y");
+            keterangan("Gunawan Prasetya", "21120119120025");
+            keterangan("Hana Adilah", "21120119120024");
+            keterangan2(41, 2);
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tugas_Modul4_Kel41/hae.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas_Modul4_Kel41
+{
+    class hae
+    {
+        public void senin()
+        {
+            String[] x = { "1. Sistem digital pada pukul 08.50-10.30", "2. Praktikum DKP pada pukul 18.00-18.50", "3. Konsep Jaringan Komputer pada pukul 20.00-21.40" };
+
+            foreach (String y in x)
+            {
+                Console.WriteLine(y);
+            }
+        }
+        public void selasa()
+        {
+            String[] x = { "1. Algoritma dan Pemprogaman pada pukul 08.50-10.30", "2. Fisika dasar 2 pada pukul 12.30-14.10" };
+
+            foreach (String y in x)
+            {
+                Console.WriteLine(y);
+            }
+        }
+        public void rabu()
+        {
+            String[] x = { "1. Kalkulus 2 pada pukul 12.40-15.10", "2. Kecakapan Antar Personal pada pukul 15.10-16.50" };
+
+            foreach (String y in x)
+            {
+                Console.WriteLine(y);
+            }
+        }
+        public void kamis()
+        {
+            String[] x = { "1. Kewarganegaraan pada pukul 08.20-10.00", "2. Metode Penelitian pada pukul 12.30-14.10" };
+            Console.WriteLine("Mata kuliah :");
+            foreach (String y in x)
+            {
+                Console.WriteLine(y);
+            }
+        }
+        public void jumat()
+        {
+            String[] x = { "1. Etika Profesi pada jam 07.00-08,40", "2. Elektonika dasar pada jam 14.00-16.30", "3. Praktimu Fisika dasar pada pukul 18.00-18.50" };
+            Console.WriteLine("Mata kuliah :");
+            foreach (String y in x)
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+
+}
